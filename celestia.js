d1afd62dfaecd39fcdc1c4fc003935cfc1bae548
ajr3021 <54864764+ajr3021@users.noreply.github.com>
2019-11-20 10:55:10 -0500

    Create celestia.js

---
+++ celestia.js
@@ -0,0 +1,28 @@
+function back_home() {
+	window.location.href = "index.html";
+}
+
+function to_settings() {
+	window.location.href = "settings.html";
+}
+function to_sign_in() {
+	window.location.href = "sing-in.html";
+}
+function to_plans() {
+	window.location.href = "PurchasePlans.html";
+}
+function to_purchase_plan() {
+	window.location.href = "purchase-plan.html";
+}
+function to_movement() {
+	window.location.href = "data-display-movement.html";
+}
+function to_decision_making() {
+	window.location.href = "data-display-decision-making.html";
+}
+function to_item_choices() {
+	window.location.href = "data-display-items.html";
+}
+function to_behavior_prediction() {
+	window.location.href = "behavior-trends.html";
+}
